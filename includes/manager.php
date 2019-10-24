91987aa8bbc98f443b719822e0527f0f7e41d243
Kevin Behrens <43488774+agapetry@users.noreply.github.com>
2019-10-24 17:42:03 -0400

    PressPermit Compat: restore Permissions menu integration
    Move Role Capabilities menu item to Permissions menu if PressPermit plugin is active (restoring previous behavior with Press Permit Core)

--- includes/manager.php
+++ includes/manager.php
@@ -288,6 +288,11 @@ class CapabilityManager
 			global $pp_admin;
 			$menu_caption = ( defined('WPLANG') && WPLANG && ( 'en_EN' != WPLANG ) ) ? __('Capabilities', 'capsman-enhanced') : 'Role Capabilities';
 			add_submenu_page( $pp_admin->get_menu('options'), __('Capability Manager', 'capsman-enhanced'),  $menu_caption, 'manage_capabilities', $this->ID, array($this, 'generalManager') );
+		
+		} elseif(did_action('presspermit_admin_menu') && function_exists('presspermit')) {
+			$menu_caption = ( defined('WPLANG') && WPLANG && ( 'en_EN' != WPLANG ) ) ? __('Capabilities', 'capsman-enhanced') : 'Role Capabilities';
+			add_submenu_page( presspermit()->admin()->getMenuParams('options'), __('Capability Manager', 'capsman-enhanced'),  $menu_caption, 'manage_capabilities', $this->ID, array($this, 'generalManager') );
+		
 		} else {
 			add_users_page( __('Capability Manager', 'capsman-enhanced'),  __('Capabilities', 'capsman-enhanced'), 'manage_capabilities', $this->ID, array($this, 'generalManager'));
 		}	
