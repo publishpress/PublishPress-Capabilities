3ad95f25e39dfb899795f3273f6091d7a2c05d93
Kevin Behrens <43488774+agapetry@users.noreply.github.com>
2021-06-10 13:06:44 -0400

    Restrictions config: organize by editor section, checkboxes for all available post types

--- includes/features-classic.php
+++ includes/features-classic.php
@@ -1,17 +1,39 @@
-<table class="wp-list-table widefat fixed striped pp-capability-menus-select editor-features-classic" <?php if (empty($_REQUEST['ppc-tab']) || ('classic' != $_REQUEST['ppc-tab'])) echo 'style="display:none;"';?>>
+<?php
+$ce_elements = PP_Capabilities_Post_Features::elementsClassic();
 
+$ce_post_disabled = [];
+
+$def_post_types = apply_filters('pp_capabilities_feature_post_types', ['post', 'page']);
+
+foreach($def_post_types as $post_type) {
+    $_disabled = get_option("capsman_feature_restrict_classic_{$post_type}", []);
+    $ce_post_disabled[$post_type] = !empty($_disabled[$default_role]) ? (array)$_disabled[$default_role] : [];
+}
+?>
+
+<table class="wp-list-table widefat fixed striped pp-capability-menus-select editor-features-classic" <?php if (empty($_REQUEST['ppc-tab']) || ('classic' != $_REQUEST['ppc-tab'])) echo 'style="display:none;"';?>>
 
     <thead>
     <tr>
         <th class="menu-column"><?php _e('Classic Editor Screen', 'capsman-enhanced') ?></th>
-        <th class="restrict-column"><?php _e('Restrict', 'capsman-enhanced') ?></th>
+
+        <?php foreach($def_post_types as $post_type) :
+            $type_obj = get_post_type_object($post_type);    
+        ?>
+            <th class="restrict-column"><?php printf(__('%s Restrict', 'capsman-enhanced'), $type_obj->labels->singular_name);?></th>
+        <?php endforeach;?>
     </tr>
     </thead>
 
     <tfoot>
     <tr>
         <th class="menu-column"><?php _e('Classic Editor Screen', 'capsman-enhanced') ?></th>
-        <th class="restrict-column"><?php _e('Restrict', 'capsman-enhanced') ?></th>
+
+        <?php foreach($def_post_types as $post_type) :
+            $type_obj = get_post_type_object($post_type);    
+        ?>
+            <th class="restrict-column"><?php printf(__('%s Restrict', 'capsman-enhanced'), $type_obj->labels->singular_name);?></th>
+        <?php endforeach;?>
     </tr>
     </tfoot>
 
@@ -20,43 +42,50 @@
 
         <td class="menu-column ppc-menu-item">
             <label for="classic-check-all-item">
-                                                        <span class="classic menu-item-link check-all-menu-link">
-                                                            <strong><i class="dashicons dashicons-leftright"></i>
-                                                            <?php _e('Toggle all', 'capsman-enhanced'); ?>
-                                                            </strong>
-                                                        </span></label>
+                <span class="classic menu-item-link check-all-menu-link">
+                    <strong><i class="dashicons dashicons-leftright"></i>
+                    <?php _e('Toggle all', 'capsman-enhanced'); ?>
+                    </strong>
+                </span></label>
         </td>
 
-        <td class="restrict-column ppc-menu-checkbox">
-            <input id="classic-check-all-item" class="check-item classic check-all-menu-item" type="checkbox"/>
-        </td>
-
+        <?php foreach($def_post_types as $post_type) :?>
+            <td class="restrict-column ppc-menu-checkbox">
+                <input id="classic-check-all-item" class="check-item classic check-all-menu-item" type="checkbox" pp_type="<?php echo $post_type;?>" />
+            </td>
+        <?php endforeach;?>
     </tr>
 
-
     <?php
+    foreach ($ce_elements as $section_title => $arr) {
+        ?>
+        <tr class="ppc-menu-row parent-menu">
+        <td colspan="<?php echo (count($def_post_types) + 1);?>"><h4 class="ppc-menu-row-section">
+        <?php echo $section_title;?>
+        </span></td>
+        </tr>
+        <?php
 
-
-    foreach ($ce_metaboxes as $index => $metabox) {
-        $sn++;
-        if ('' !== $metabox) {
+        foreach ($arr as $feature_slug => $arr_feature) {
+            if (!$feature_slug) {
+                continue;
+            }
             ?>
             <tr class="ppc-menu-row parent-menu">
                 <td class="menu-column ppc-menu-item">
-
-                    <label for="check-item-<?php echo $sn; ?>">
-<span class="classic menu-item-link<?php echo (in_array($metabox, $ce_post_disabled)) ? ' restricted' : ''; ?>">
-<strong><i class="dashicons dashicons-arrow-right"></i>
-<?php echo $ce_metaboxes_names[$index]; ?>
-</strong></span>
-                    </label>
+                    <span class="classic menu-item-link<?php echo (in_array($feature_slug, $ce_post_disabled['post'])) ? ' restricted' : ''; ?>">
+                    <strong><i class="dashicons dashicons-arrow-right"></i>
+                        <?php echo $arr_feature['label']; ?>
+                    </strong></span>
                 </td>
 
-                <td class="restrict-column ppc-menu-checkbox">
-                    <input id="check-item-<?php echo $sn; ?>" class="check-item" type="checkbox"
-                           name="capsman_feature_ce_post_disabled[]"
-                           value="<?php echo $metabox; ?>"<?php echo (in_array($metabox, $ce_post_disabled)) ? ' checked' : ''; ?> />
-                </td>
+                <?php foreach($def_post_types as $post_type) :?>
+                    <td class="restrict-column ppc-menu-checkbox">
+                        <input id="cb_<?php echo $post_type . '-' . str_replace(['#', '.'], '_', $feature_slug);?>" class="check-item" type="checkbox"
+                                name="capsman_feature_restrict_classic_<?php echo $post_type;?>[]"
+                                value="<?php echo $feature_slug; ?>" <?php checked(in_array($feature_slug, $ce_post_disabled[$post_type]));?> />
+                    </td>
+                <?php endforeach;?>
             </tr>
             <?php
         }
