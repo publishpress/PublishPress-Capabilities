178d882a71aba200109059f1c8dd01cff34437bd
Valentín García <htmgarcia@gmail.com>
2021-11-17 17:42:54 -0600

    Update constant name

--- capsman-enhanced.php
+++ capsman-enhanced.php
@@ -147,7 +147,7 @@ if (!cme_is_plugin_active('press-permit-core.php') && !cme_is_plugin_active('pre
 	define('CAPSMAN_PERMISSIONS_INSTALLED', false);
 
 	// Sidebar banners library
-	if (!defined('PP_WP_BANNERS_LOADED')) {
+	if (!defined('PP_WP_BANNERS_VERSION')) {
 	    require_once __DIR__ . '/vendor/publishpress/wordpress-banners/banners.php';
 	}
 } else {
