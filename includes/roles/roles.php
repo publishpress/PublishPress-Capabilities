138992513599443ac89a50995bc8c2a52203e68f
Kevin Behrens <43488774+agapetry@users.noreply.github.com>
2021-12-21 23:31:26 -0500

    Escape output variables

--- includes/roles/roles.php
+++ includes/roles/roles.php
@@ -1,11 +1,11 @@
 <div class="wrap publishpress-caps-manage pressshack-admin-wrapper pp-capability-roles-wrapper">
 
     <div class="wrap">
-        <h1 class="wp-heading-inline"><?php _e('Roles', 'capsman-enhanced') ?> </h1>
+        <h1 class="wp-heading-inline"><?php esc_html_e('Roles', 'capsman-enhanced') ?> </h1>
         <?php
         if (isset($_REQUEST['s']) && $search = esc_attr(wp_unslash($_REQUEST['s']))) {
             /* translators: %s: search keywords */
-            printf(' <span class="subtitle">' . __('Search results for &#8220;%s&#8221;', 'capsman-enhanced') . '</span>', $search);
+            printf(' <span class="subtitle">' . esc_html__('Search results for &#8220;%s&#8221;', 'capsman-enhanced') . '</span>', $search);
         }
 
         //the roles table instance
@@ -16,13 +16,13 @@
         <hr class="wp-header-end">
         <div id="ajax-response"></div>
         <form class="search-form wp-clearfix" method="get">
-            <?php $table->search_box(__('Search Roles', 'capsman-enhanced'), 'roles'); ?>
+            <?php $table->search_box(esc_html__('Search Roles', 'capsman-enhanced'), 'roles'); ?>
         </form>
         <div id="col-container" class="wp-clearfix">
             <div id="col-left">
                 <div class="col-wrap">
                     <div class="form-wrap">
-                        <h2><?php _e('Add New Role', 'capsman-enhanced') ?> </h2>
+                        <h2><?php esc_html_e('Add New Role', 'capsman-enhanced') ?> </h2>
                         <form id="addrole" method="post" action="" class="validate">
                             <input type="hidden" name="action" value="pp-roles-add-role">
                             <input type="hidden" name="screen" value="<?php echo 'capabilities_page_pp-capabilities-roles'; /*get_current_screen()->id*/ ?>">
@@ -30,14 +30,14 @@
                             wp_nonce_field('add-role');
                             ?>
                             <div class="form-field form-required">
-                                <label for="name"><?php _e('Name', 'capsman-enhanced') ?> </label>
+                                <label for="name"><?php esc_html_e('Name', 'capsman-enhanced') ?> </label>
                                 <input name="name" id="name" type="text" value="" size="40">
-                                <p><?php _e('The name is how it appears on your site.', 'capsman-enhanced'); ?></p>
+                                <p><?php esc_html_e('The name is how it appears on your site.', 'capsman-enhanced'); ?></p>
                             </div>
 
                             <p class="submit">
                                 <input type="submit" name="submit" id="submit" class="button button-primary"
-                                       value="<?php _e('Add'); ?> ">
+                                       value="<?php esc_html_e('Add'); ?> ">
                             </p>
                         </form>
                     </div>
@@ -49,7 +49,7 @@
                         <?php $table->display(); //Display the table ?>
                     </form>
                     <div class="form-wrap edit-term-notes">
-                        <p><?php __('Description here.', 'capsman-enhanced') ?></p>
+                        <p><?php esc_html__('Description here.', 'capsman-enhanced') ?></p>
                     </div>
                 </div>
             </div>
