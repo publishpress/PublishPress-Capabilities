3476e52f0d564d03625bc60ab5d1df851debb262
Kevin Behrens <43488774+agapetry@users.noreply.github.com>
2022-01-13 13:01:51 -0500

    Sanitization consistency

--- includes/settings-handler.php
+++ includes/settings-handler.php
@@ -9,7 +9,7 @@
 add_action('init', function() {
     if (check_admin_referer('pp-capabilities-settings') && current_user_can('manage_capabilities')) {
         if (!empty($_POST['all_options'])) {
-	        foreach(array_map('sanitize_key', explode(',', $_POST['all_options'])) as $option_name) {
+	        foreach(array_map('sanitize_key', explode(',', sanitize_text_field($_POST['all_options']))) as $option_name) {
 	            foreach (['cme_', 'capsman', 'pp_capabilities'] as $prefix) {
 	                if (0 === strpos($option_name, $prefix)) {
 						
