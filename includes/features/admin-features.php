efa9a15ca206b6bc38b076fd3a68412b262d9017
Kevin Behrens <43488774+agapetry@users.noreply.github.com>
2023-02-15 11:35:46 -0500

    Remove "Click here" captions from buttons

--- includes/features/admin-features.php
+++ includes/features/admin-features.php
@@ -243,7 +243,7 @@ $admin_features_elements = PP_Capabilities_Admin_Features::elementsLayout();
                                     esc_html__( 'PublishPress Permissions is 100% free to install.', 'capsman-enhanced' )
                                 ),
                                 admin_url( 'plugin-install.php?s=publishpress-ppcore-install&tab=search&type=term' ),
-                                esc_html__( 'Click here to install PublishPress Permissions', 'capsman-enhanced' ),
+                                esc_html__( 'Install PublishPress Permissions', 'capsman-enhanced' ),
                                 'install-permissions.jpg'
                             );
                             ?>
