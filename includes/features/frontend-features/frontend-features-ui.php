9088841e68e362fcf3ffb75dcd5d721aa8464066
Kevin Behrens <43488774+agapetry@users.noreply.github.com>
2023-06-29 12:08:51 -0400

    Frontend Features: Default to Hide IDs tab

--- includes/features/frontend-features/frontend-features-ui.php
+++ includes/features/frontend-features/frontend-features-ui.php
@@ -103,8 +103,8 @@ class PP_Capabilities_Frontend_Features_UI
                             <div class="frontend-element-toggle">
                                 <div class="ppc-button-group"
                                     data-hide-selector=".frontend-features-toggle">
-                                    <label class="element-classes"><input type="radio" name="frontend_feature_active" value=".frontend-element-classes"><?php esc_html_e('Hide IDs or Classes', 'capsman-enhanced'); ?></label>
-                                    <label class="custom-css selected"><input type="radio" name="frontend_feature_active" value=".frontend-element-styles" checked> <?php esc_html_e('Add Custom CSS', 'capsman-enhanced'); ?></label>
+                                    <label class="element-classes selected"><input type="radio" name="frontend_feature_active" value=".frontend-element-classes" checked><?php esc_html_e('Hide IDs or Classes', 'capsman-enhanced'); ?></label>
+                                    <label class="custom-css"><input type="radio" name="frontend_feature_active" value=".frontend-element-styles"> <?php esc_html_e('Add Custom CSS', 'capsman-enhanced'); ?></label>
                                     <label class="body-class"><input type="radio" name="frontend_feature_active" value=".frontend-element-bodyclass"> <?php esc_html_e('Add Body Class', 'capsman-enhanced'); ?></label>
                                 </div>
                             </div>
