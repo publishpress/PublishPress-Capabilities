3e288cc431d970b9823a988579761a1b227546f0
olatechpro <olawale.olatech.pro@gmail.com>
2023-10-19 11:35:38 +0100

    - Update old-fashioned tooltips #818

--- common/js/admin.dev.js
+++ common/js/admin.dev.js
@@ -5,7 +5,7 @@ jQuery(document).ready( function($) {
 	$('a.neg-type-caps').attr('title',cmeAdmin.typeCapsNegationCaption);
 	//$('td.cap-unreg').attr('title',cmeAdmin.typeCapUnregistered);
 	$('a.normal-cap').attr('title',cmeAdmin.switchableCaption);
-	$('span.cap-x:not([class*="pp-cap-key"])').attr('title',cmeAdmin.capNegated);
+	$('span.cap-x:not([class*="pp-cap-key"])').html(cmeAdmin.capNegated);
 	$('table.cme-checklist input[class!="cme-check-all"]').not(':disabled').attr('title',cmeAdmin.chkCaption);
 
   if ($('.ppc-checkboxes-documentation-link').length > 0) {
