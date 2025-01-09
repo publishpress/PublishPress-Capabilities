c3ffe4fa595210cb09b1a843a92ddf5fe82d241f
DAnn2012 <dannunziata@hotmail.it>
2025-01-09 12:25:03 +0100

    Update roles.php
    Completely removed unused placeholder

--- includes/roles/roles.php
+++ includes/roles/roles.php
@@ -29,9 +29,6 @@
                 <form action="" method="post">
                     <?php $table->display(); //Display the table ?>
                 </form>
-                <div class="form-wrap edit-term-notes">
-                    <p><?php esc_html_e('Description here.', 'capability-manager-enhanced') ?></p>
-                </div>
             </div>
         </div>
         <form method="get">
